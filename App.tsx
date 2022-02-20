d3d480f792b41843d72d14eae71d2c165fa63178
Travis gun kim <gunget@naver.com>
2022-02-20 18:07:47 +0900

    220220 splash screen 적용
    1. figma에서 간단하게 icon, splash screen image 만듦
    2. expo-splash-screen설치 후 app.json, app.tsx변경
    3. splash screen 적용 완료
    4. 다음과제는 selections의 배경이미지 만들기 및 구분 명확하게 꾸미기

--- App.tsx
+++ App.tsx
@@ -5,17 +5,16 @@ import {
   RouteProp,
 } from '@react-navigation/native'
 import {
-  CardStyleInterpolators,
+  // CardStyleInterpolators,
   createStackNavigator,
   StackNavigationProp,
 } from '@react-navigation/stack'
 import {SafeAreaProvider} from 'react-native-safe-area-context'
 
 import {useFonts} from 'expo-font'
-// import * as SplashScreen from 'expo-splash-screen'
-import {dummyPlaceType} from './constants/dummy'
+import * as SplashScreen from 'expo-splash-screen'
 
-import {Selections, TelOnAir, MNGRpt, MNGCam} from './screens'
+import {Selections} from './screens'
 import Tabs from './navigations/tabs'
 
 export type RootStackParamList = {
@@ -32,7 +31,45 @@ export type TabsRouteProp = RouteProp<RootStackParamList, 'Tabs'>
 const Stack = createStackNavigator<RootStackParamList>()
 
 //Splash Screen 제어하지 않고 앱띄우기
+// const App: FunctionComponent = () => {
+//   const [fontsLoaded] = useFonts({
+//     'Roboto-Black': require('./assets/fonts/Roboto-Black.ttf'),
+//     'Roboto-Bold': require('./assets/fonts/Roboto-Bold.ttf'),
+//     'Roboto-Regular': require('./assets/fonts/Roboto-Regular.ttf'),
+//     'CarmenSans-Thin': require('./assets/fonts/CarmenSans-Thin.otf'),
+//     'CarmenSans-SemiBold': require('./assets/fonts/CarmenSans-SemiBold.otf'),
+//     'CarmenSans-Regular': require('./assets/fonts/CarmenSans-Regular.otf'),
+//   })
+
+//   if (!fontsLoaded) {
+//     return null
+//   }
+
+//   return (
+//     <SafeAreaProvider>
+//       <NavigationContainer>
+//         <Stack.Navigator
+//           screenOptions={{
+//             headerShown: false,
+//           }}
+//           initialRouteName={'Selections'}>
+//           <Stack.Screen name="Selections" component={Selections} />
+//           <Stack.Screen name="Tabs" component={Tabs} />
+//           {/* <Stack.Screen name="TelOnAir" component={TelOnAir} />
+//           <Stack.Screen name="MNGRpt" component={MNGRpt} />
+//           <Stack.Screen name="MNGCam" component={MNGCam} /> */}
+//         </Stack.Navigator>
+//       </NavigationContainer>
+//     </SafeAreaProvider>
+//   )
+// }
+
+// export default App
+
+// splash screen으로 띄우기
+
 const App: FunctionComponent = () => {
+  const [appIsReady, setAppIsReady] = useState(false)
   const [fontsLoaded] = useFonts({
     'Roboto-Black': require('./assets/fonts/Roboto-Black.ttf'),
     'Roboto-Bold': require('./assets/fonts/Roboto-Bold.ttf'),
@@ -42,7 +79,19 @@ const App: FunctionComponent = () => {
     'CarmenSans-Regular': require('./assets/fonts/CarmenSans-Regular.otf'),
   })
 
-  if (!fontsLoaded) {
+  useEffect(() => {
+    if (!fontsLoaded) {
+      SplashScreen.preventAutoHideAsync()
+    } else {
+      // setTimeout(() => {
+      //   SplashScreen.hideAsync()
+      // }, 2000)
+      SplashScreen.hideAsync()
+      setAppIsReady(true)
+    }
+  }, [fontsLoaded])
+
+  if (!appIsReady) {
     return null
   }
 
@@ -52,6 +101,8 @@ const App: FunctionComponent = () => {
         <Stack.Navigator
           screenOptions={{
             headerShown: false,
+            // gestureDirection: 'horizontal',
+            // cardStyleInterpolator: CardStyleInterpolators.forHorizontalIOS,
           }}
           initialRouteName={'Selections'}>
           <Stack.Screen name="Selections" component={Selections} />
@@ -66,47 +117,3 @@ const App: FunctionComponent = () => {
 }
 
 export default App
-
-//   const [appIsReady, setAppIsReady] = useState(false)
-//   const [fontsLoaded] = useFonts({
-//     'Roboto-Black': require('./assets/fonts/Roboto-Black.ttf'),
-//     'Roboto-Bold': require('./assets/fonts/Roboto-Bold.ttf'),
-//     'Roboto-Regular': require('./assets/fonts/Roboto-Regular.ttf'),
-//   })
-
-//   useEffect(() => {
-//     if (!fontsLoaded) {
-//       SplashScreen.preventAutoHideAsync()
-//     } else {
-//       // setTimeout(() => {
-//       //   SplashScreen.hideAsync()
-//       // }, 2000)
-//       SplashScreen.hideAsync()
-//       setAppIsReady(true)
-//     }
-//   }, [fontsLoaded])
-
-//   if (!appIsReady) {
-//     return null
-//   }
-
-//   return (
-//     <SafeAreaProvider>
-//       <NavigationContainer>
-//         <Stack.Navigator
-//           screenOptions={{
-//             headerShown: false,
-//             gestureDirection: 'horizontal',
-//             cardStyleInterpolator: CardStyleInterpolators.forHorizontalIOS,
-//           }}
-//           initialRouteName={'Tabs'}>
-//           <Stack.Screen name="Tabs" component={Tabs} />
-
-//           <Stack.Screen name="Place" component={Place} />
-//         </Stack.Navigator>
-//       </NavigationContainer>
-//     </SafeAreaProvider>
-//   )
-// }
-
-// export default App
